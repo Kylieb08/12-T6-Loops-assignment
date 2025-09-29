bd5bbbcb4ca53f02460977704f57dc4c26545ca8
339780520 <339780520@2260I-L29-008>
2025-09-29 08:24:48 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _12_T6_Loops_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
