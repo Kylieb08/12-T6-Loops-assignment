8b918f8dc3c3cee00187508982eaae0c5322fede
339780520 <339780520@2260I-L29-008>
2025-09-29 09:10:40 -0400

    added menu, started banking

--- Program.cs
+++ Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Prompter();
+            int choice;
+
+            Console.WriteLine("Which program would you like to run? (Enter the number)");
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine("1. Prompter");
+            Console.WriteLine("2. Banking");
+            Console.WriteLine("3. Doubles Roller");
+            
+            while (!Int32.TryParse(Console.ReadLine(), out choice) || choice < 1 || choice > 3)
+            {
+                Console.WriteLine("Invalid input, please try again");
+            }
+
+            switch (choice)
+            {
+                case 1:
+                    Prompter();
+                    break;
+
+                case 2:
+                    Banking();
+                    break;
+            }
         }
 
         public static void Prompter()
@@ -31,7 +53,11 @@
 
         public static void Banking()
         {
+            double balance = 150;
 
+            Console.WriteLine("Welcome to Bank of Blorb ATM");
+            Console.WriteLine(" What would you like to do?");
+            Console.WriteLine("----------------------------");
         }
     }
 }
