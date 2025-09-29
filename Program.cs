8f025834c7a33fb71adaf4bf883d7b93b9cf9fd5
339780520 <339780520@2260I-L29-008>
2025-09-29 08:41:12 -0400

    done prompter

--- Program.cs
+++ Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Prompter();
+        }
+
+        public static void Prompter()
+        {
+            int min, max, num;
+
+            Console.WriteLine("Please enter an integer for a minimum value");
+            while (!Int32.TryParse(Console.ReadLine(), out min))
+                Console.WriteLine("Invalid input, please try again");
+
+            Console.WriteLine();
+            Console.WriteLine("Please enter an integer for a maximum value");
+            while (!Int32.TryParse(Console.ReadLine(),out max) || max <= min)
+                Console.WriteLine("Invalid input, please try again");
+
+            Console.WriteLine();
+            Console.WriteLine($"Please enter an integer in between {min} and {max}");
+            while (!Int32.TryParse(Console.ReadLine(), out num) || num < min || num > max)
+                Console.WriteLine("Invalid input, please try again");
+
+            Console.WriteLine();
+            Console.WriteLine($"You entered {num}, which is between {min} and {max}");
         }
     }
 }
